872a774be738be6d53c334827de6908c848e4df0
Kunal Raut <kunal.r@prodapt.com>
2024-05-02 14:48:21 +0530

    added on scroll on articles and margin bottom on project

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,14 +3,14 @@ import React from 'react';
 export const ProjectCard = ({ title, description, imgUrl, link}) => {
 
   return (
-    <div className="w-full">
+    <div className="w-full mb-4">
       <a href={link} target="_blank" rel="noreferrer">
         <div>
           <img className="w-full h-auto" src={imgUrl} alt={title} /> {/* Added h-auto to ensure proper image height */}
           <div className="mt-2">
             <strong className="text-grey-900">{title}</strong>
             <br />
-            <span className="text-grey-600 mt-2">{description}</span>
+            <span className="text-grey-600 mt-2 mb-5">{description}</span>
           </div>
         </div>
       </a>
