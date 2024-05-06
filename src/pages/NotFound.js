f6d71db964d768dd086d5872d7a1281d4e4914a2
kunalraut666 <kunalraut489@gmail.com>
2024-05-06 23:52:22 +0530

    added margin in not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -7,7 +7,7 @@ export default function NotFound() {
       <h1 className='sm:text-4xl text-2xl font-bold my-6 text-grey-900 text-center'>
           Page not found
       </h1>
-      <img src={pageNotFoundImg} alt='pagenotfound'/>
+      <img src={pageNotFoundImg} alt='pagenotfound' className='mb-10'/>
     </>
   )
 }
